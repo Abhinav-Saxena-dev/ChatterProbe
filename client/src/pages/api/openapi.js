f647e3a55cdb587ecf5ed93d848d3c849dd42db0
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2023-03-31 10:30:05 +0530

    minor fixes

--- client/src/pages/api/openapi.js
+++ client/src/pages/api/openapi.js
@@ -1,9 +1,15 @@
 import { Configuration, OpenAIApi } from "openai";
+import { config } from "dotenv";
+
+config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPEN_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+
+
 
 const handler = async (req, res) => {
   try {
