99b026b7a75b263d540c55ced722a21e13761287
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2023-07-27 21:03:58 +0530

    removing typo

--- client/src/Components/Cards/card.component.jsx
+++ client/src/Components/Cards/card.component.jsx
@@ -40,7 +40,7 @@ const Card = ({title, snippet, link}) => {
         }}
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Scrapper Read (BETA)
+        Scraper Read (BETA)
         <svg
           aria-hidden="true"
           className="w-4 h-4 ml-2 -mr-1"
